ee36660f6b86e58a9786d790427846c1c86789f9
Jagaa <jagaa1103@gmail.com>
2020-11-15 11:59:41 -0800

    Lab07 is done

---
+++ Lab07/ascii.js
@@ -0,0 +1,115 @@
+(function() {
+
+    window.onload = function() {
+        const DEFAULT_INTERVAL = 250;
+    
+        // set defaults
+        let speed = DEFAULT_INTERVAL;
+        let frameLength = 0;
+        let index = 0;
+        let startInterval = null;
+        let isRunning = false;
+        let firstFrame = null;
+        let frame = null;
+    
+        // DOM elements
+        let textArea = document.getElementById("text-area");
+        let startBtn = document.getElementById("start");
+        let stopBtn = document.getElementById("stop");
+        let animation = document.getElementById("animation");
+        let speedBox = document.getElementById("turbo");
+        let fontSize = document.getElementById("fontsize");
+    
+        // change animation
+        function changeAnimation() {
+            let allFrames = ANIMATIONS[animation.value];
+            frame = allFrames.split("=====\n");
+            frameLength = frame.length;
+            firstFrame = frame[0];
+            textArea.value = firstFrame;
+            index = 0;
+        }
+        
+        function changeSize() {
+            switch(fontSize.value) {
+                case "Tiny":
+                    textArea.className = "tiny";
+                    break;
+                case "Small": 
+                    textArea.className = "small";
+                    break;
+                case "Large":
+                    textArea.className = "large";
+                    break;
+                case "Extra Large":
+                    textArea.className = "xl";
+                    break;
+                case "XXL":
+                    textArea.className = "xxl";
+                    break;
+                default:
+                    textArea.className = "medium";
+                    break;
+            }
+        }
+    
+        function changeSpeed() {
+            speed = speedBox.checked ? 50 : DEFAULT_INTERVAL;
+            if (isRunning) {
+                startAnimation();
+            }
+        }
+    
+        // start animation
+        function startAnimation() {
+            if (startInterval) {
+                clearInterval(startInterval);
+            }
+            if (frame === "" || frame === null) {
+                firstFrame = null;
+                frame = null;
+                return;
+            }
+            startInterval = setInterval(function() {
+                textArea.value = frame[index++];
+                if (index === frameLength) {
+                    index = 0;
+                }
+            }, speed);
+            isRunning = true;
+        }
+    
+        // stop animation
+        function stopAnimation() {
+            if (!startInterval) {
+                return;
+            }
+            clearInterval(startInterval);
+            textArea.value = firstFrame;
+            isRunning = false;
+        }
+    
+        // start 
+        function onStart() {
+            startBtn.disabled = true;
+            stopBtn.disabled = false;
+            startAnimation();
+        }
+    
+        // stop 
+        function onStop() {
+            startBtn.disabled = false;
+            stopBtn.disabled = true;
+            index = 0;
+            stopAnimation();
+        }
+    
+        startBtn.onclick = onStart;
+        stopBtn.onclick = onStop;
+        animation.onchange = changeAnimation;
+        speedBox.onchange = changeSpeed;
+        fontSize.onchange = changeSize;
+        
+    };
+    
+    })();
